agustin-rather/mx-sdk-py-cli

commit 9e2b61b1b4dfb25b81a784dac9c9f89bb404cdf4

make logger colourful

multiversx_sdk_cli/cli.py
@@ -4,6 +4,8 @@ import sys
 from argparse import ArgumentParser
 from typing import Any, List
 
+from rich.logging import RichHandler
+
 import multiversx_sdk_cli.cli_accounts
 import multiversx_sdk_cli.cli_block
 import multiversx_sdk_cli.cli_config
@@ -37,16 +39,14 @@ def main(cli_args: List[str] = sys.argv[1:]):
 
 
 def _do_main(cli_args: List[str]):
-    logging.basicConfig(level=logging.INFO)
-
     argv_with_config_args = config.add_config_args(cli_args)
     parser = setup_parser(argv_with_config_args)
     args = parser.parse_args(argv_with_config_args)
 
     if args.verbose:
-        logging.basicConfig(level=logging.DEBUG, force=True)
+        logging.basicConfig(level="DEBUG", force=True, format='%(name)s: %(message)s', handlers=[RichHandler(show_time=False, rich_tracebacks=True)])
     else:
-        logging.basicConfig(level=logging.INFO, force=True)
+        logging.basicConfig(level="INFO", format='%(name)s: %(message)s', handlers=[RichHandler(show_time=False, rich_tracebacks=True)])
 
     if not hasattr(args, "func"):
         parser.print_help()

commit ee1f7a3e3409740bc12520f14976566162b1771a

Make log handler output to stderr instead of stdout
Fixes bug introduced by commit 9e2b61b1 where `mxpy data parse`
output was mixed with log messages

multiversx_sdk_cli/cli.py
@@ -5,6 +5,7 @@ from argparse import ArgumentParser
 from typing import Any, List
 
 from rich.logging import RichHandler
+from rich.console import Console
 
 import multiversx_sdk_cli.cli_accounts
 import multiversx_sdk_cli.cli_config
@@ -43,10 +44,11 @@ def _do_main(cli_args: List[str]):
     parser = setup_parser(argv_with_config_args)
     args = parser.parse_args(argv_with_config_args)
 
+    rich_handler = RichHandler(show_time=False, rich_tracebacks=True, console=Console(stderr=True))
     if args.verbose:
-        logging.basicConfig(level="DEBUG", force=True, format='%(name)s: %(message)s', handlers=[RichHandler(show_time=False, rich_tracebacks=True)])
+        logging.basicConfig(level="DEBUG", force=True, format='%(name)s: %(message)s', handlers=[rich_handler])
     else:
-        logging.basicConfig(level="INFO", format='%(name)s: %(message)s', handlers=[RichHandler(show_time=False, rich_tracebacks=True)])
+        logging.basicConfig(level="INFO", format='%(name)s: %(message)s', handlers=[rich_handler])
 
     verify_deprecated_entries_in_config_file()
 